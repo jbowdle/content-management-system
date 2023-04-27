439bbc4592c447267573be850cce933d19863557
jbowdle <johnwbowdle@gmail.com>
2023-04-26 21:01:18 -0500

    created menu

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // REMINDER: use .env to hide mysql password
+const inquirer = require("inquirer");
+
+inquirer
+    .prompt([
+        {
+            type: "list",
+            name: "menu",
+            message: "What would you like to do?",
+            choices: [
+                "View all employees",
+                "View all employees by department",
+                "View all employees by manager",
+                "Add employee",
+                "Remove employee",
+                "Update employee role",
+                "Update employee manager",
+                "View all roles",
+                "Add role",
+                "Remove role",
+                "View all departments",
+                "Add department",
+                "Remove department",
+                "View total utilized budget by department",
+                "Quit"
+            ]
+
+        }
+    ])
+    .then((response) => {
+        // placeholder
+        console.log(`You chose: ${response.menu}`);
+    });
