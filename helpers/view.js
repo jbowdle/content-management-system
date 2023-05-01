f5c2e9d4b91dc69bcc0bf2056d7ffa9804c96c08
jbowdle <johnwbowdle@gmail.com>
2023-04-30 23:45:02 -0500

    added create table functionality using cli-table3

--- helpers/view.js
+++ helpers/view.js
@@ -1,4 +1,5 @@
 const mysql = require("mysql2");
+const { createTable } = require("./createTable");
 
 require("dotenv").config();
 
@@ -15,7 +16,8 @@ const viewEmployees = function () {
         if (err) {
             console.log(err);
         }
-        console.log(results);
+
+        createTable(results);
     });
 }
 
